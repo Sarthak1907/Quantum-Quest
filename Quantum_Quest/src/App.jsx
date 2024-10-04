46b62685cb68709fdd5c2769f92dbc927ec0f0bb
Sarthak1907 <sarthaksrivastava230@outlook.com>
2024-10-04 19:10:47 +0530

    Added Sidebar, Header, Footer to code

--- Quantum_Quest/src/App.jsx
+++ Quantum_Quest/src/App.jsx
@@ -1,5 +1,16 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
+import "bootstrap/dist/css/bootstrap.css";
+
 const App = () => {
-  return <></>;
+  return (
+    <>
+      <Sidebar></Sidebar>
+      <Header></Header>
+      <Footer></Footer>
+    </>
+  );
 };
 
 export default App;
