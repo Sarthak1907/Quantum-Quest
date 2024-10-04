0ad86d7205f76bd4baad986cba5cb45285b39c47
Sarthak1907 <sarthaksrivastava230@outlook.com>
2024-10-04 19:47:48 +0530

    Formatting of the Home Page

--- Quantum_Quest/src/App.jsx
+++ Quantum_Quest/src/App.jsx
@@ -2,13 +2,18 @@ import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
 import "bootstrap/dist/css/bootstrap.css";
+import "./App.css";
 
 const App = () => {
   return (
     <>
-      <Sidebar></Sidebar>
-      <Header></Header>
-      <Footer></Footer>
+      <div className="app_container">
+        <Sidebar></Sidebar>
+        <div className="content">
+          <Header></Header>
+          <Footer></Footer>
+        </div>
+      </div>
     </>
   );
 };
--- Quantum_Quest/src/components/Sidebar.jsx
+++ Quantum_Quest/src/components/Sidebar.jsx
@@ -2,12 +2,12 @@ const Sidebar = () => {
   return (
     <>
       <div
-        className="d-flex flex-column flex-shrink-0 p-3 bg-body-tertiary"
+        className="d-flex flex-column flex-shrink-0 p-3 text-bg-dark sidebar"
         style={{ width: "280px" }}
       >
         <a
           href="/"
-          className="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none"
+          className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none"
         >
           <svg className="bi pe-none me-2" width="40" height="32">
             <use xlinkHref="#bootstrap"></use>
@@ -25,7 +25,7 @@ const Sidebar = () => {
             </a>
           </li>
           <li>
-            <a href="#" className="nav-link link-body-emphasis">
+            <a href="#" className="nav-link text-white">
               <svg className="bi pe-none me-2" width="16" height="16">
                 <use xlinkHref="#speedometer2"></use>
               </svg>
@@ -33,7 +33,7 @@ const Sidebar = () => {
             </a>
           </li>
           <li>
-            <a href="#" className="nav-link link-body-emphasis">
+            <a href="#" className="nav-link text-white">
               <svg className="bi pe-none me-2" width="16" height="16">
                 <use xlinkHref="#table"></use>
               </svg>
@@ -41,7 +41,7 @@ const Sidebar = () => {
             </a>
           </li>
           <li>
-            <a href="#" className="nav-link link-body-emphasis">
+            <a href="#" className="nav-link text-white">
               <svg className="bi pe-none me-2" width="16" height="16">
                 <use xlinkHref="#grid"></use>
               </svg>
@@ -49,7 +49,7 @@ const Sidebar = () => {
             </a>
           </li>
           <li>
-            <a href="#" className="nav-link link-body-emphasis">
+            <a href="#" className="nav-link text-white">
               <svg className="bi pe-none me-2" width="16" height="16">
                 <use xlinkHref="#people-circle"></use>
               </svg>
@@ -61,7 +61,7 @@ const Sidebar = () => {
         <div className="dropdown">
           <a
             href="#"
-            className="d-flex align-items-center link-body-emphasis text-decoration-none dropdown-toggle"
+            className="d-flex align-items-center text-white text-decoration-none dropdown-toggle"
             data-bs-toggle="dropdown"
             aria-expanded="false"
           >
