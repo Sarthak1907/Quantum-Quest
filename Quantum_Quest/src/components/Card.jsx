285d116e0302a0cee1e8c320f4df7465fbb8bfb6
Sarthak1907 <sarthaksrivastava230@outlook.com>
2024-10-05 18:57:11 +0530

    Added HeroContent and Card to the code

---
+++ Quantum_Quest/src/components/Card.jsx
@@ -0,0 +1,24 @@
+const Card = () => {
+  return (
+    <>
+      <div className="card" style={{ width: "18rem" }}>
+        <img src="..." className="card-img-top" alt="..." />
+        <div className="card-body">
+          <p className="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+          <button
+            type="button"
+            className="btn btn-secondary"
+            style={{ color: "#E3E7AF" }}
+          >
+            Add to Cart
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Card;
